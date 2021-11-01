5bdebe83c8d16a3329b751626dbf3d89bb3303b1
duyhung311 <ldhung311>
2021-11-01 22:07:52 +0700

    Fix interface of checkout page

--- src/VisaPayment.js
+++ src/VisaPayment.js
@@ -1,50 +1,45 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery';
-import style from './App.css'
 
 const VisaPayment = (props) => {
-
-    console.log(props.price.TotalPrice);
-
     return (
-        <div className="tab-pane fade show active"> 
+        <div className="tab-pane fade show active" style={{margin:'20px'}}> 
             <div className="tab-single"> 
                 <form className="form-card">
                     <div className="row">
                         <div className="col-12">
-                            <div className="form-group" > 
-                                <label className="form-control-placeholder p-0" htmlFor="number">CardNumber</label>
+                            <div className="form-group" >   
                                 <input type="text" className="form-control p-0" id="number" required />  
+                                <label className="form-control-placeholder p-0" htmlFor="number">CardNumber</label>
                             </div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-12">
                             <div className="form-group"> 
-                                <label className="form-control-placeholder p-0" htmlFor="name">Cardholder'sName</label>
                                 <input type="text" className="form-control p-0" id="name" required />
+                                <label className="form-control-placeholder p-0" htmlFor="name">Cardholder'sName</label>
                             </div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-sm-4 col-12">
-                            <div className="form-group">
-                                <label className="form-control-placeholder p-0" htmlFor="sdate">StartDate</label>
+                            <div className="form-group"> 
                                 <input type="text" className="form-control p-0" id="sdate" required /> 
-                    
+                                <label className="form-control-placeholder p-0" htmlFor="sdate">StartDate</label>
                             </div>
                         </div>
                         <div className="col-sm-4 col-12">
                             <div className="form-group"> 
-                                <label className="form-control-placeholder p-0" htmlFor="expdate">ExpirationDate</label> 
                                 <input type="text" className="form-control p-0" id="expdate" required />
+                                <label className="form-control-placeholder p-0" htmlFor="expdate">ExpirationDate</label> 
                             </div>
                         </div>
                         <div className="col-sm-4 col-12">
-                            <div className="form-group">     
-                                <label className="form-control-placeholder p-0" htmlFor="passw">CVV</label> 
+                            <div className="form-group"> 
                                 <input type="password" className="form-control p-0" id="passw" required />
+                                <label className="form-control-placeholder p-0" htmlFor="passw">CVV</label>
                             </div>
                         </div>
                     </div>
@@ -56,12 +51,12 @@ const VisaPayment = (props) => {
                             <h4>$ {props.price}</h4>
                         </div>
                     </div>
-                    <div className="row mb-2">
-                        <div className="col-sm-12"> 
-                            <button type="button" className="btn btn-primary btn-block" >
-                                Pay 
-                            </button>
-                        </div>
+                    <div className=" row mb-2">
+                            
+                        <button type="button" className="btn btn-sm btn-danger rounded-pill"  >
+                            Pay 
+                        </button>
+                        
                     </div>
                 </form>
             </div>
